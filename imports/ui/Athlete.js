4b0367eaa8710d29c2cb1f7b05cc9f68fc0db58e
Linas <l.spukas@gmail.com>
2017-08-25 14:24:18 +0300

    Add delete button

--- imports/ui/Athlete.js
+++ imports/ui/Athlete.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Athletes } from '../api/athletes';
+
 // Task component - represents a single todo item
-const Athlete = ({ athlete }) => (
-  <li>{athlete.name}</li>
-);
+const Athlete = ({ athlete }) => {
+  const handleSubmit = () => Athletes.remove(athlete._id);
+
+  return (
+    <li>
+      {athlete.name}
+      <button onClick={handleSubmit}>Delete</button>
+    </li>
+  );
+};
 
 Athlete.propTypes = {
   athlete: PropTypes.shape({
